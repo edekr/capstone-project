28def52af580a26cee37bd0323848920660fb663
edekr <ekrotter@mailbox.org>
2022-07-26 13:58:06 +0200

    Add s3 bucket creation

---
+++ infrastructure/setup_terraform_state_bucket.sh
@@ -0,0 +1,15 @@
+TERRAFORM_S3_BUCKET_NAME_1=myweatherbucket-1234567
+TERRAFORM_S3_BUCKET_NAME_2=myspotifybucket-1234567
+if aws s3 ls "s3://$TERRAFORM_S3_BUCKET_NAME_1" 2>&1 | grep -q 'NoSuchBucket'
+then
+    aws s3api create-bucket --bucket $TERRAFORM_S3_BUCKET_NAME_1 --region eu-central-1 --create-bucket-configuration LocationConstraint=eu-central-1
+else
+    echo "terraform bucket already exist"
+fi
+
+if aws s3 ls "s3://$TERRAFORM_S3_BUCKET_NAME_2" 2>&1 | grep -q 'NoSuchBucket'
+then
+    aws s3api create-bucket --bucket $TERRAFORM_S3_BUCKET_NAME_2 --region eu-central-1 --create-bucket-configuration LocationConstraint=eu-central-1
+else
+    echo "terraform bucket already exist"
+fi
