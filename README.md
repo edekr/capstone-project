53741007966451340a10adcd24e0f53b57c93c5e
edekr <ekrotter@mailbox.org>
2022-07-26 10:06:02 +0200

    Init terraform

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # capstone-project
+
+
+## Goal
+The goal is to fetch the spotify listening history and also the weather data with the information from the metadata inside a picture.
+
+![Overview](img/image.png)
---
+++ infrastructure/database.tf
@@ -0,0 +1,14 @@
+resource "aws_dynamodb_table" "weather" {
+  name           = "Weather"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 20
+  write_capacity = 20
+  hash_key       = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+
+
+}
